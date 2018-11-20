788ac1d288927e9860ba84764e66584c5b595f7e
glonngren <glonngren@hawk.iit.edu>
2018-11-20 14:00:00 -0600

    Map Fix
    API fix

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ function initMap(){
   var ola = {lat: 41.8313506, lng: -87.62705470000002};
   var map = new google.maps.Map(
     document.getElementById('map'), {
-    zoom: 16, center: ola
+    zoom: 4, center: ola
     }
   );
   var marker = new google.maps.Marker({
